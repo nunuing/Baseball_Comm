4ee0e7f75e05f7c902f91ea7890ed2e6d4f27d45
eycho <chw3872@gmail.com>
2023-08-14 21:12:38 +0900

    routing

--- BaseballComm/BaseballComm/urls.py
+++ BaseballComm/BaseballComm/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import BaseballComm.views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', BaseballComm.views.index, name='index'),
 ]
